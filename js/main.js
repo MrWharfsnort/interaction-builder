bdbc87e6b4d0efec3d47f5ae8899ea422b7f297a
Nate Farnsworth <nfarnsworth@gmail.com>
2014-06-19 21:33:40 +0000

    working copy

--- js/main.js
+++ js/main.js
@@ -1,37 +1,55 @@
-$(function() {
+$().ready(function () {
+  $("#results").hide();
 
-  $("#submit-btn").click(function(e) {
-    e.preventDefault();
-    var cpniVal = $("#cpni").val();
-    var custVal = $("#custName").val();
-    var creditVal = $("#creditCheck").val();
-    console.log(cpniVal);
-    var cc1 = "======= $$$$ - SALES DEPT MACRO - $$$$ =======";
-    var cc2 = "*CPNI VERIFIED: " + cpniVal;
-    var cc3 = "*CALLER'S FULL NAME: " + custVal;
-    var cc4 = "*PERMISSION TO RUN CREDIT CHECK: " + creditVal; 
-    $("#results").empty().text(cc1 +"\n"+ cc2 +"\n" + cc3 + "\n" + cc4);
+  $(function () {
+    $("#installDate").datetimepicker({
+      pickTime: false
+    });
+    $("#expirationDate").datetimepicker({
+      pickTime: false
+    });
   });
 
-  $("#reset-btn").click(function(e) {
+  $("#submit-btn").click(function (e) {
+    e.preventDefault();
+    $("#results").show();
+
+    var cpniVerified = $("#cpniVerified").val();
+    var custName = $("#custName").val();
+    var creditCheck = $("#creditCheck").val();
+    var firstBill = $("#firstBill").val();
+    var monthlyService = $("#monthlyService").val();
+    var depositAmount = $("#depositAmount").val();
+    var installAmount = $("#installAmount").val();
+    var equipmentFees = $("#equipmentFees").val();
+    var expirationDate = $("#expirationDate").val();
+    var installType = $("#installType").val();
+    var contactNumber = $("#contactNumber").val();
+    var installDate = $("#installDate").val();
+
+    var cc1 = "====$$$$=== SALES DEPT MACRO ===$$$$====\n";
+    var cc2 = "* CPNI VERIFIED: " + cpniVerified + "\n";
+    var cc3 = "* CALLER'S FULL NAME: " + custName + "\n";
+    var cc4 = "* PERMISSION TO RUN CREDIT CHECK: " + creditCheck + "\n";
+    var cc5 = "* 1ST BILL W/ PRORATE (COD): " + firstBill + "\n";
+    var cc6 = "* MONTHLY SERVICE RATE: " + monthlyService + "\n";
+    var cc7 = "* DEPOSIT AMOUNT: " + depositAmount + "\n";
+    var cc8 = "* INSTALL AMOUNT: " + installAmount + "\n";
+    var cc9 = "* EQUIPMENT FEES: " + equipmentFees + "\n";
+    var cc10 = "* CAMPAIGN/PACKAGE EXP DATE: " + expirationDate + "\n";
+    var cc11 = "* INSTALLATION TYPE: " + installType + "\n";
+    var cc12 = "* CTM #: " + contactNumber + "\n";
+    var cc13 = "* INSTALL DATE & TIME: " + installDate + "\n";
+    var cc14 = "========================================";
+
+    $("#results").empty().text(cc1 + cc2 + cc3 + cc4 + cc5 + cc6 +
+      cc7 + cc8 + cc9 + cc10 + cc11 + cc12 + cc13 + cc14);
+  });
+
+  $("#reset-btn").click(function (e) {
+    e.preventDefault();
     $("input").val('');
-    $("#results").empty();
+    $("#results").empty().hide();
   });
 
 });
-
-
-// ====$$$$=== SALES DEPT MACRO ===$$$$====
-// *CPNI - Y/N:
-// *CALLER'S FULL NAME:
-// *PERMISSION TO RUN CREDIT CHECK - Y/N:
-// *1ST BILL W/PRORATE (COD):
-// *MONTHLY SERVICE:
-// *DEPOSIT AMOUNT:
-// *INSTALL AMOUNT:
-// *EQUIPMENT FEES:
-// *CAMPAIGN/PACKAGE EXP DATE:
-// *PRO INSTALL/EXPRESS CONNECT:
-// *CTM #:
-// *INSTALL DATE & TIME:
-// ==============================================
