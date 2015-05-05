93b87a3ed67fbc086bedb73aa77ff91af74569b5
Nate Farnsworth <nfarnsworth@gmail.com>
2015-05-05 20:34:36 +0000

    moved all js code to main.js and enabled copy to clipboard

--- js/main.js
+++ js/main.js
@@ -1,31 +1,65 @@
-// Hide the #results box on ready
+
 $(function () {
-  $('#resultContainer').hide();
+  $('#resultContainer, :disabled').hide();
 
+  $('input, textarea').placeholder(); // HTML5 Placeholder - https://github.com/mathiasbynens/jquery-placeholder
 
-  $(function() {
-  // Enable placeholder function for IE
-  
-    $('input, textarea').placeholder(); // HTML5 Placeholder - https://github.com/mathiasbynens/jquery-placeholder
-
-  // Bootstrap DateTimePicker
   // http://eonasdan.github.io/bootstrap-datetimepicker
-    $('#installDate, #expirationDate, #billDate, #seasonalStart, #seasonalEnd, #nextHold, #discoDate').datetimepicker({
-      pickTime: false
-    });
-    $('#callTime').datetimepicker({
-      getCurrentTime: true
-    });
-
-  // jQuery MaskedInput
-  // https://github.com/digitalBush/jquery.maskedinput
-    $('#contactNumber').mask('(999) 999-9999', {placeholder: '(___) ___-____'});
+  $('#installDate, #expirationDate, #billDate, #seasonalStart, #seasonalEnd, #nextHold, #discoDate').datetimepicker({
+    pickTime: false
   });
 
-  // http://api.jquery.com/event.preventdefault/
-  $('#resetBtn').click(function (e) {
+  $('#callTime').datetimepicker({
+    getCurrentTime: true
+  });
+
+  // https://github.com/digitalBush/jquery.maskedinput
+  $('#contactNumber').mask('(999) 999-9999', {placeholder: '(___) ___-____'});
+
+
+  function getValues() {
+    var formData = $('form').serializeArray();
+
+    $.each(formData, function(i, field) {
+      if (!field.value) {
+        this.value = 'n/a';
+      } else {
+        var fieldValues = field.name + ': ' + field.value + ',  ';
+        $('#results').append(fieldValues);
+        $('#resultContainer').show();
+      }
+    });
+  }
+
+  $('button').click(function (e) {
     e.preventDefault();
+  });
+
+  $('#submitBtn').click(function() {
+    $('#results').empty();
+    getValues();
+    $('#results').text($('#results').text().replace(/,\s\s$/g, ''));
+  });
+
+  $('#resetBtn').click(function () {
     $('input, select').val('');
     $('#resultContainer').hide();
   });
+
+  // zeroclipboard config
+  var client = new ZeroClipboard( $('#copyBtn'), {
+    moviePath: 'js/ZeroClipboard.swf'
+  });
+
+  client.on('copy', function() {
+    $('#copyBtn').text('copied!').addClass('btn-success has-success');
+  });
+
+  client.on('aftercopy', function() {
+    setTimeout(function() {
+      $('#copyBtn').text('copy to clipboard').removeClass('btn-success has-success');
+    }, 200);
+    $('#results').effect('highlight', 200);
+  });
+
 });
